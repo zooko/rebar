7c3b699226a4170d8d817f83a4711d8e3bed4fe9
Zsomething <nothanks@ontheemail.com>
2026-01-04 13:55:46 -0700

    add bench-allocators.sh

---
+++ bench-allocators.sh
@@ -0,0 +1,12 @@
+echo CPU type:
+
+# print out CPU type on macos
+sysctl -n machdep.cpu.brand_string 2>/dev/null
+
+# print out CPU type on linux
+grep "model name" /proc/cpuinfo | uniq 2>/dev/null
+
+cargo build --release
+./target/release/rebar build -e '^rust/regex(-(s|mi|sn|je|rp)malloc)?$'
+./target/release/rebar measure -e '^rust/regex(-(s|mi|sn|je|rp)malloc)?$' -f curated | tee res.csv
+./target/release/rebar rank res.csv
