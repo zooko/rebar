907c4d09d2b1305d1d2631dc9f211d4551bfafae
Zsomething <nothanks@ontheemail.com>
2026-01-22 23:33:58 -0700

    fix bug in bench script

--- bench-allocators.sh
+++ bench-allocators.sh
@@ -22,6 +22,8 @@ OUTPUT_DIR="${OUTPUT_DIR:-./benchmark-results}/${CPUSTR_DOT_OSSTR}"
 RESF="${OUTPUT_DIR}/${BNAME}.result.txt"
 GRAPHF="${OUTPUT_DIR}/${BNAME}.graph.svg"
 
+mkdir -p ${OUTPUT_DIR}
+
 echo "# Saving result into \"${RESF}\""
 echo "# Saving graph into \"${GRAPHF}\""
 rm -f $RESF $GRAPHF
